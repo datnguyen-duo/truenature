f7babe03b937e2f7dd8ef28645233e06bb058e1e
Tihomir Pavlovic <tihomir.pavlovic1112@gmail.com>
2021-11-15 16:31:06 +0100

    qa

--- js/global.js
+++ js/global.js
@@ -251,102 +251,116 @@ jQuery(window).on("load", function () {
         SMOOTH SCROLL START
         --------------------------------------------------------------------------------- */
 
-  function smoothScroll() {
-    smoothScroll("#scroll-container"); // declare container here
+    function smoothScroll() {
+      smoothScroll("#scroll-container"); // declare container here
 
-    function smoothScroll(content, viewport, smoothness) {
-      content = gsap.utils.toArray(content)[0];
-      smoothness = smoothness || 2;
+      function smoothScroll(content, viewport, smoothness) {
+        content = gsap.utils.toArray(content)[0];
+        smoothness = smoothness || 2;
 
-      gsap.set(viewport || content.parentNode, {
-        overflow: "hidden",
-        position: "fixed",
-        height: "100%",
-        width: "100%",
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-      });
-      gsap.set(content, { overflow: "visible", width: "100%" });
+        gsap.set(viewport || content.parentNode, {
+          overflow: "hidden",
+          position: "fixed",
+          height: "100%",
+          width: "100%",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+        });
+        gsap.set(content, { overflow: "visible", width: "100%" });
 
-      let getProp = gsap.getProperty(content),
-        setProp = gsap.quickSetter(content, "y", "px"),
-        setScroll = ScrollTrigger.getScrollFunc(window),
-        removeScroll = () => (content.style.overflow = "visible"),
-        killScrub = (trigger) => {
-          let scrub = trigger.getTween
-            ? trigger.getTween()
-            : gsap.getTweensOf(trigger.animation)[0];
-          scrub && scrub.kill();
-          trigger.animation.progress(trigger.progress);
-        },
-        height,
-        isProxyScrolling;
+        let getProp = gsap.getProperty(content),
+          setProp = gsap.quickSetter(content, "y", "px"),
+          setScroll = ScrollTrigger.getScrollFunc(window),
+          removeScroll = () => (content.style.overflow = "visible"),
+          killScrub = (trigger) => {
+            let scrub = trigger.getTween
+              ? trigger.getTween()
+              : gsap.getTweensOf(trigger.animation)[0];
+            scrub && scrub.kill();
+            trigger.animation.progress(trigger.progress);
+          },
+          height,
+          isProxyScrolling;
 
-      function onResize() {
-        height = content.clientHeight;
-        content.style.overflow = "visible";
-        document.body.style.height = height + "px";
+        function onResize() {
+          height = content.clientHeight;
+          content.style.overflow = "visible";
+          document.body.style.height = height + "px";
+        }
+        onResize();
+        ScrollTrigger.addEventListener("refreshInit", onResize);
+        ScrollTrigger.addEventListener("refresh", () => {
+          removeScroll();
+          requestAnimationFrame(removeScroll);
+        });
+        ScrollTrigger.defaults({ scroller: content });
+        ScrollTrigger.prototype.update = (p) => p;
+
+        ScrollTrigger.scrollerProxy(content, {
+          scrollTop(value) {
+            if (arguments.length) {
+              isProxyScrolling = true;
+              setProp(-value);
+              setScroll(value);
+              return;
+            }
+            return -getProp("y");
+          },
+          getBoundingClientRect() {
+            return {
+              top: 0,
+              left: 0,
+              width: window.innerWidth,
+              height: window.innerHeight,
+            };
+          },
+        });
+
+        return ScrollTrigger.create({
+          animation: gsap.fromTo(
+            content,
+            { y: 0 },
+            {
+              y: () => document.documentElement.clientHeight - height,
+              ease: "none",
+              onUpdate: ScrollTrigger.update,
+            }
+          ),
+          scroller: window,
+          invalidateOnRefresh: true,
+          start: 0,
+          end: () => height - document.documentElement.clientHeight,
+          scrub: smoothness,
+          onUpdate: (self) => {
+            if (isProxyScrolling) {
+              killScrub(self);
+              isProxyScrolling = false;
+            }
+          },
+          onRefresh: killScrub,
+        });
       }
-      onResize();
-      ScrollTrigger.addEventListener("refreshInit", onResize);
-      ScrollTrigger.addEventListener("refresh", () => {
-        removeScroll();
-        requestAnimationFrame(removeScroll);
-      });
-      ScrollTrigger.defaults({ scroller: content });
-      ScrollTrigger.prototype.update = (p) => p;
-
-      ScrollTrigger.scrollerProxy(content, {
-        scrollTop(value) {
-          if (arguments.length) {
-            isProxyScrolling = true;
-            setProp(-value);
-            setScroll(value);
-            return;
-          }
-          return -getProp("y");
-        },
-        getBoundingClientRect() {
-          return {
-            top: 0,
-            left: 0,
-            width: window.innerWidth,
-            height: window.innerHeight,
-          };
-        },
-      });
-
-      return ScrollTrigger.create({
-        animation: gsap.fromTo(
-          content,
-          { y: 0 },
-          {
-            y: () => document.documentElement.clientHeight - height,
-            ease: "none",
-            onUpdate: ScrollTrigger.update,
-          }
-        ),
-        scroller: window,
-        invalidateOnRefresh: true,
-        start: 0,
-        end: () => height - document.documentElement.clientHeight,
-        scrub: smoothness,
-        onUpdate: (self) => {
-          if (isProxyScrolling) {
-            killScrub(self);
-            isProxyScrolling = false;
-          }
-        },
-        onRefresh: killScrub,
-      });
     }
-  }
 
-  smoothScroll();
+    var windowWidthSmoothScroll = jQuery(window).width()
 
-/*	-----------------------------------------------------------------------------
-    SMOOTH SCROLL END
---------------------------------------------------------------------------------- */
+    jQuery(window).on('resize', function(){
+      var windowWidthSmoothScroll = jQuery(window).width()
+
+      if(windowWidthSmoothScroll > 1100){
+        smoothScroll();
+      }
+    });
+
+    if(windowWidthSmoothScroll > 1100){
+      smoothScroll();
+    }
+
+    
+
+  /*	-----------------------------------------------------------------------------
+      SMOOTH SCROLL END
+  --------------------------------------------------------------------------------- */
 });
