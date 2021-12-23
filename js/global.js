5b4787ecdfb511718b5c37381443ee753aec4ac1
Dat Nguyen <dat@duo-studio.co>
2021-12-22 20:20:42 -0500

    client edits

--- js/global.js
+++ js/global.js
@@ -1,7 +1,6 @@
 (function ($) {
   //document ready
   $(document).ready(function () {
-
     $(".single_question").on("click", function (event) {
       $(this).toggleClass("active");
       $(this).find("p").slideToggle();
@@ -102,7 +101,7 @@
       sessionStorage.setItem("loader", "true");
       var tl = gsap.timeline({
         onComplete: function () {
-          // console.log("complete");
+          gsap.set(loader_content, { pointerEvents: "none" });
         },
       });
 
@@ -251,114 +250,112 @@ jQuery(window).on("load", function () {
         SMOOTH SCROLL START
         --------------------------------------------------------------------------------- */
 
-    function smoothScroll() {
-      smoothScroll("#scroll-container"); // declare container here
+  function smoothScroll() {
+    smoothScroll("#scroll-container"); // declare container here
 
-      function smoothScroll(content, viewport, smoothness) {
-        content = gsap.utils.toArray(content)[0];
-        smoothness = smoothness || 2;
+    function smoothScroll(content, viewport, smoothness) {
+      content = gsap.utils.toArray(content)[0];
+      smoothness = smoothness || 2;
 
-        gsap.set(viewport || content.parentNode, {
-          overflow: "hidden",
-          position: "fixed",
-          height: "100%",
-          width: "100%",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-        });
-        gsap.set(content, { overflow: "visible", width: "100%" });
+      gsap.set(viewport || content.parentNode, {
+        overflow: "hidden",
+        position: "fixed",
+        height: "100%",
+        width: "100%",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+      });
+      gsap.set(content, { overflow: "visible", width: "100%" });
 
-        let getProp = gsap.getProperty(content),
-          setProp = gsap.quickSetter(content, "y", "px"),
-          setScroll = ScrollTrigger.getScrollFunc(window),
-          removeScroll = () => (content.style.overflow = "visible"),
-          killScrub = (trigger) => {
-            let scrub = trigger.getTween
-              ? trigger.getTween()
-              : gsap.getTweensOf(trigger.animation)[0];
-            scrub && scrub.kill();
-            trigger.animation.progress(trigger.progress);
-          },
-          height,
-          isProxyScrolling;
+      let getProp = gsap.getProperty(content),
+        setProp = gsap.quickSetter(content, "y", "px"),
+        setScroll = ScrollTrigger.getScrollFunc(window),
+        removeScroll = () => (content.style.overflow = "visible"),
+        killScrub = (trigger) => {
+          let scrub = trigger.getTween
+            ? trigger.getTween()
+            : gsap.getTweensOf(trigger.animation)[0];
+          scrub && scrub.kill();
+          trigger.animation.progress(trigger.progress);
+        },
+        height,
+        isProxyScrolling;
 
-        function onResize() {
-          height = content.clientHeight;
-          content.style.overflow = "visible";
-          document.body.style.height = height + "px";
-        }
-        onResize();
-        ScrollTrigger.addEventListener("refreshInit", onResize);
-        ScrollTrigger.addEventListener("refresh", () => {
-          removeScroll();
-          requestAnimationFrame(removeScroll);
-        });
-        ScrollTrigger.defaults({ scroller: content });
-        ScrollTrigger.prototype.update = (p) => p;
-
-        ScrollTrigger.scrollerProxy(content, {
-          scrollTop(value) {
-            if (arguments.length) {
-              isProxyScrolling = true;
-              setProp(-value);
-              setScroll(value);
-              return;
-            }
-            return -getProp("y");
-          },
-          getBoundingClientRect() {
-            return {
-              top: 0,
-              left: 0,
-              width: window.innerWidth,
-              height: window.innerHeight,
-            };
-          },
-        });
-
-        return ScrollTrigger.create({
-          animation: gsap.fromTo(
-            content,
-            { y: 0 },
-            {
-              y: () => document.documentElement.clientHeight - height,
-              ease: "none",
-              onUpdate: ScrollTrigger.update,
-            }
-          ),
-          scroller: window,
-          invalidateOnRefresh: true,
-          start: 0,
-          end: () => height - document.documentElement.clientHeight,
-          scrub: smoothness,
-          onUpdate: (self) => {
-            if (isProxyScrolling) {
-              killScrub(self);
-              isProxyScrolling = false;
-            }
-          },
-          onRefresh: killScrub,
-        });
+      function onResize() {
+        height = content.clientHeight;
+        content.style.overflow = "visible";
+        document.body.style.height = height + "px";
       }
+      onResize();
+      ScrollTrigger.addEventListener("refreshInit", onResize);
+      ScrollTrigger.addEventListener("refresh", () => {
+        removeScroll();
+        requestAnimationFrame(removeScroll);
+      });
+      ScrollTrigger.defaults({ scroller: content });
+      ScrollTrigger.prototype.update = (p) => p;
+
+      ScrollTrigger.scrollerProxy(content, {
+        scrollTop(value) {
+          if (arguments.length) {
+            isProxyScrolling = true;
+            setProp(-value);
+            setScroll(value);
+            return;
+          }
+          return -getProp("y");
+        },
+        getBoundingClientRect() {
+          return {
+            top: 0,
+            left: 0,
+            width: window.innerWidth,
+            height: window.innerHeight,
+          };
+        },
+      });
+
+      return ScrollTrigger.create({
+        animation: gsap.fromTo(
+          content,
+          { y: 0 },
+          {
+            y: () => document.documentElement.clientHeight - height,
+            ease: "none",
+            onUpdate: ScrollTrigger.update,
+          }
+        ),
+        scroller: window,
+        invalidateOnRefresh: true,
+        start: 0,
+        end: () => height - document.documentElement.clientHeight,
+        scrub: smoothness,
+        onUpdate: (self) => {
+          if (isProxyScrolling) {
+            killScrub(self);
+            isProxyScrolling = false;
+          }
+        },
+        onRefresh: killScrub,
+      });
     }
+  }
 
-    var windowWidthSmoothScroll = jQuery(window).width()
+  var windowWidthSmoothScroll = jQuery(window).width();
 
-    jQuery(window).on('resize', function(){
-      var windowWidthSmoothScroll = jQuery(window).width()
+  jQuery(window).on("resize", function () {
+    var windowWidthSmoothScroll = jQuery(window).width();
 
-      if(windowWidthSmoothScroll > 1100){
-        smoothScroll();
-      }
-    });
-
-    if(windowWidthSmoothScroll > 1100){
+    if (windowWidthSmoothScroll > 1100) {
       smoothScroll();
     }
+  });
 
-    
+  if (windowWidthSmoothScroll > 1100) {
+    smoothScroll();
+  }
 
   /*	-----------------------------------------------------------------------------
       SMOOTH SCROLL END
